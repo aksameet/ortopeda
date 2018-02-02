a349c71045ca838eda922889b4f6d39bf5706df7
aksameet <janusz_jaskowicz@yahoo.com>
2018-02-02 18:00:07 +0100

    About and Obszary section update

--- src/reducers/reducer_obszary.js
+++ src/reducers/reducer_obszary.js
@@ -32,9 +32,7 @@ export default () => [
             return (
                 <div className="obszary-content-description">
                     <h3>Artroskopia stawu kolanowego</h3>
-                    <p>
-                        Artroskopię kolana najczęściej wykonuje się po urazach stawu kolanowego (uszkodzenie łąkotek, uszkodzenie więzadeł krzyżowych, złamaniach kostno-chrzęstnych), w przypadku przewlekłych dolegliwości kolana (liczne wysięki stawu kolanowego, choroby chrząstki stawowej, choroby błony maziowej), celem leczenia stanu zapalnego kolana, celem usuwania ciał wolnych kolana.
-                    </p>
+                    <p>Artroskopię kolana najczęściej wykonuje się po urazach stawu kolanowego (uszkodzenie łąkotek, uszkodzenie więzadeł krzyżowych, złamaniach kostno-chrzęstnych), w przypadku przewlekłych dolegliwości kolana (liczne wysięki stawu kolanowego, choroby chrząstki stawowej, choroby błony maziowej), celem leczenia stanu zapalnego kolana, celem usuwania ciał wolnych kolana.</p>
                 </div>
             )
         }
@@ -46,9 +44,8 @@ export default () => [
             return (
                 <div className="obszary-content-description">
                     <h3>Taping</h3>
-                    <p>
-                        Plastrowanie (inaczej taping, nalepianie plastra) - metoda leczenia poprzez aplikację na ciele, w ściśle określony sposób, nierozciągliwego lub elastycznego plastra. Nalepiony plaster jest źródłem bodźców wyłącznie mechanicznych (nie jest nasączony żadnym lekiem) i w zależności od rodzaju, sposobu i miejsca aplikacji, ma różnorakie oddziaływanie. Jedną z metod tapingu jest plastrowanie dynamiczne. Taping znajduje zastosowanie w fizjoterapii przeciwbólowej, ortopedycznej, sportowej, neurologicznej, pediatrycznej, pooperacyjnej, ginekologicznej, onkologicznej.
-                    </p>
+                    <p>Plastrowanie (inaczej taping, nalepianie plastra) - metoda leczenia poprzez aplikację na ciele, w ściśle określony sposób, nierozciągliwego lub elastycznego plastra. Nalepiony plaster jest źródłem bodźców wyłącznie mechanicznych (nie jest nasączony żadnym lekiem) i w zależności od rodzaju, sposobu i miejsca aplikacji, ma różnorakie oddziaływanie. Jedną z metod tapingu jest plastrowanie dynamiczne.</p>
+                    <p>Taping znajduje zastosowanie w fizjoterapii przeciwbólowej, ortopedycznej, sportowej, neurologicznej, pediatrycznej, pooperacyjnej, ginekologicznej, onkologicznej.</p>
                 </div>
             )
         }
@@ -60,9 +57,8 @@ export default () => [
             return (
                 <div className="obszary-content-description">
                     <h3>Medycyna Manualna</h3>
-                    <p>
-                        Terapia manualna polega na badaniu i leczeniu stawów obwodowych i kręgosłupa. Pierwszą częścią badania jest zazwyczaj dokładny wywiad. Następnie dokonuje się badania jakości ruchu, jego zakresów, biomechaniki oraz innych czynników (np. bolesności). Podczas terapii za pomocą specjalnych technik mobilizacji lub manipulacji i pracy na tkankach miękkich przywraca się zaburzoną biomechanikę stawów (odblokowanie stawów), co prowadzi do zmniejszenia bólu, zwiększenia zakresu ruchomości, ustąpienia stanów zapalnych.
-                    </p>
+                    <p>Terapia manualna polega na badaniu i leczeniu stawów obwodowych i kręgosłupa. Pierwszą częścią badania jest zazwyczaj dokładny wywiad. Następnie dokonuje się badania jakości ruchu, jego zakresów, biomechaniki oraz innych czynników (np. bolesności).</p>
+                    <p>Podczas terapii za pomocą specjalnych technik mobilizacji lub manipulacji i pracy na tkankach miękkich przywraca się zaburzoną biomechanikę stawów (odblokowanie stawów), co prowadzi do zmniejszenia bólu, zwiększenia zakresu ruchomości, ustąpienia stanów zapalnych.</p>
                 </div>
             )
         }
@@ -74,9 +70,8 @@ export default () => [
             return (
                 <div className="obszary-content-description">
                     <h3>Badanie Bioderek niemowląt</h3>
-                    <p>
-                        Do ortopedy dziecięcego dziecko powinno trafić zanim skończy 3 miesiące. Podczas badania ortopeda sprawdza stopień i równomierność odwodzenia bioder, ocenia długość nóżek, dotykiem bada stawy. Obecnie, nawet gdy nic nie wzbudza podejrzeń, zleca się profilaktycznie wykonanie usg stawów biodrowych u dzieci. To badanie jest całkowicie bezpieczne, można je wykonywać noworodkom. Jeśli lekarz stwierdzi, że niedorozwój stawów jest nieznaczny, zaleca jedynie odpowiednią pielęgnację niemowlaka, co daje szansę na samoistny zanik wady. Jeżeli dysplazja zostanie stwierdzona, nie można zwlekać, ale od razu należy rozpocząć leczenie. Wadę trzeba skorygować zanim niemowlę podejmie próbę chodzenia.
-                    </p>
+                    <p>Do ortopedy dziecięcego dziecko powinno trafić zanim skończy 3 miesiące. Podczas badania ortopeda sprawdza stopień i równomierność odwodzenia bioder, ocenia długość nóżek, dotykiem bada stawy. Obecnie, nawet gdy nic nie wzbudza podejrzeń, zleca się profilaktycznie wykonanie usg stawów biodrowych u dzieci. </p>
+                    <p>To badanie jest całkowicie bezpieczne, można je wykonywać noworodkom. Jeśli lekarz stwierdzi, że niedorozwój stawów jest nieznaczny, zaleca jedynie odpowiednią pielęgnację niemowlaka, co daje szansę na samoistny zanik wady. Jeżeli dysplazja zostanie stwierdzona, nie można zwlekać, ale od razu należy rozpocząć leczenie. Wadę trzeba skorygować zanim niemowlę podejmie próbę chodzenia.</p>
                 </div>
             )
         }
