3b8510122994472309008fd5244711767ca72a9b
Janusz Jaskowicz <januszjaskowicz1@Januszs-MacBook-Pro.local>
2018-12-27 19:32:32 +0100

    Dec 27 #4

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     render() {
         return (
             <div className='footer' >
-                Copyright <img src={Copyright} alt=''/> 2018 Janusz Jaskowicz . All rights Reserved.
+                Copyright <img src={Copyright} alt=''/> 2018 Janusz Jaskowicz. All rights Reserved.
             </div>
         )
     }
